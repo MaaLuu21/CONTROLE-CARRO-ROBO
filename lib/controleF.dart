cc76fb11072bcf5dc1cfc12510550de83b02a6e6
malu <marialuizapaes22@gmail.com>
2023-12-28 14:25:25 -0300

    atualizações

--- lib/controleF.dart
+++ lib/controleF.dart
@@ -1,7 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:math'; // Importe a biblioteca dart:math
+import 'dart:math';
 import 'botaoTriangular.dart';
 
 class ControleTela extends StatefulWidget {
@@ -21,80 +20,125 @@ class _ControleTelaState extends State<ControleTela> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: TriangularButton(
-                      size: 110,
-                      onPressed: () {
-                        print('Botão triangular pressionado!');
-                      },
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 40),
+                          child: TriangularButton(
+                            size: 110,
+                            onPressed: () {
+                              print('Botão triangular pressionado!');
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 40, top: 20),
+                          child: Transform(
+                            alignment: Alignment.center,
+                            transform: Matrix4.rotationZ(pi),
+                            child: TriangularButton(
+                              size: 110,
+                              onPressed: () {
+                                print('Botão triangular pressionado de ponta cabeça!');
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, top: 20),
-                    child: Transform(
-                      alignment: Alignment.center,
-                      transform: Matrix4.rotationZ(pi),
-                      child: TriangularButton(
-                        size: 110,
-                        onPressed: () {
-                          print('Botão triangular pressionado de ponta cabeça!');
-                        },
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Row(
+                        //ADICONAR BOTÕES SECUNDARIOS PARA OUTRAS FORMAS DE CURVA DO CARRO
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 20),
+                            child: Transform(
+                              alignment: Alignment.center,
+                              transform: Matrix4.rotationZ(-pi/2),
+                              child: TriangularButton(
+                                size: 110,
+                                onPressed: () {
+                                  print('Botão triangular pressionado de ponta cabeça!');
+                                },
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 20),
+                            child: Transform(
+                              alignment: Alignment.center,
+                              transform: Matrix4.rotationZ(pi/2),
+                              child: TriangularButton(
+                                size: 110,
+                                onPressed: () {
+                                  print('Botão triangular pressionado de ponta cabeça!');
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ],
               ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+              height: 60,
+              width: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Expanded(
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: Transform(
-                        alignment: Alignment.center,
-                        transform: Matrix4.rotationZ(-pi/2),
-                        child: TriangularButton(
-                          size: 110,
-                          onPressed: () {
-                            print('Botão triangular pressionado de ponta cabeça!');
-                          },
+                 //Deixar dinâmico os container, verde claro quando estiver deligado, e vermelho claro quando estiver desligado!!!!!!!!!!!!!!!!!!!
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 255, 51, 0),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: Transform(
-                        alignment: Alignment.center,
-                        transform: Matrix4.rotationZ(pi/2),
-                        child: TriangularButton(
-                          size: 110,
-                          onPressed: () {
-                            print('Botão triangular pressionado de ponta cabeça!');
-                          },
-                        ),
+                    SizedBox(width: 10,),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 47, 255, 0),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       backgroundColor: Color.fromARGB(255, 28, 28, 28),
     );
